7341655895420ca94f0653e8191c3a9d11506753
sadiqgr <sadiq@growtomation.in>
2026-01-16 00:38:15 +0530

    Add homepage route

--- index.js
+++ index.js
@@ -13,6 +13,29 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const customObjectUrl = 'https://api.hubspot.com/crm/v3/objects/2-224020006?properties=name,bio,breed';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    
+    try {
+        const resp = await axios.get(customObjectUrl, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { 
+            title: 'Pet Dashboard', 
+            data: data 
+        });
+    } catch (error) {
+        console.error('Error fetching custom objects:', error);
+        res.render('homepage', { 
+            title: 'Pet Dashboard', 
+            data: [],
+            error: 'Failed to load data'
+        });
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
